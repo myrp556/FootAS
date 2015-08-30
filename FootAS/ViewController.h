3a62e0a60b4f372b6974df2fb6736a0ac9fae578
Yuntao Wang <Osea@Yuntaos-MacBook-Air.local>
2015-08-30 11:31:57 +0800

    Initial Commit

---
+++ FootAS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FootAS
+//
+//  Created by Yuntao Wang on 15/8/30.
+//  Copyright (c) 2015年 Yuntao Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
